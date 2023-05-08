6d83d26ee39394b9e315b1c17ade561bfc507763
Abhishek Pandey <abhishekpandey3188@gmail.com>
2023-05-09 03:51:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import MatrixPage from "./pages/Matrix";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Prevent from "./Prevent";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Deadend from "./pages/Deadend";
+import Solved from "./pages/Solved";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-[#0F0F0F] h-screen">
+        {/* <MatrixPage /> */}
+        <Routes>
+          <Route path="/welcome" element={<MatrixPage />} />
+          <Route
+            path="/"
+            exact
+            element={
+              <Prevent>
+                <Home />
+              </Prevent>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <Prevent>
+                <About />
+              </Prevent>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/deadend" element={<Deadend />} />
+          <Route path="/solved" element={<Solved />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
